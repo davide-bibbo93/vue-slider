d47d4d01bedd89e626e9f55971aa115f22bf40cf
Davide <bibbodavide@gmail.com>
2021-01-27 16:18:18 +0100

    fatti pallini circle

--- js/main.js
+++ js/main.js
@@ -11,20 +11,23 @@ new Vue({
     ],
   },
   methods: {
-   nextPhoto: function () {
-     this.photoIndex += 1;
+    nextPhoto: function () {
+      this.photoIndex += 1;
 
-     if(this.photoIndex > (this.photos.length - 1)){
-       this.photoIndex = 0;
-     }
-   },
-   prevPhoto: function () {
-     this.photoIndex -= 1;
+      if(this.photoIndex > (this.photos.length - 1)){
+        this.photoIndex = 0;
+      }
+    },
+    prevPhoto: function () {
+      this.photoIndex -= 1;
 
-     if(this.photoIndex < (0)){
-       this.photoIndex = this.photos.length - 1;
-     }
-   }
+      if(this.photoIndex < (0)){
+        this.photoIndex = this.photos.length - 1;
+      }
+    },
+    setCircle: function (index) {
+      this.photoIndex = index;
+    }
   }
 });
 Vue.config.devtools = true;
