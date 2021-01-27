0d0f73465135a1b8fe245dcb112a3e04ba98eb63
Davide <bibbodavide@gmail.com>
2021-01-27 14:42:11 +0100

    caricati files originali, modificati e fatte funzioni prev e next

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// Ricreare lo slider già fatto in jquery con Vue.js.
+new Vue({
+  el: "#app",
+  data: {
+    photoIndex: 0,
+    photos: [
+      "img/img1.jpg",
+      "img/img2.jpg",
+      "img/img3.jpg",
+      "img/img4.jpg"
+    ],
+  },
+  methods: {
+   nextPhoto: function () {
+     this.photoIndex += 1;
+
+     if(this.photoIndex > (this.photos.length - 1)){
+       this.photoIndex = 0;
+     }
+   },
+   prevPhoto: function () {
+     this.photoIndex -= 1;
+
+     if(this.photoIndex < (0)){
+       this.photoIndex = this.photos.length - 1;
+     }
+   }
+  }
+});
+Vue.config.devtools = true;
